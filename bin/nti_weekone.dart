23f9a2fed88c1db94a1af988abc17fa8a5116788
Your Name <karenarmanuos@gmail.com>
2026-03-31 15:42:24 +0200

    first commit

---
+++ bin/nti_weekone.dart
@@ -0,0 +1,5 @@
+import 'package:nti_weekone/nti_weekone.dart' as nti_weekone;
+
+void main(List<String> arguments) {
+  print('Hello world: ${nti_weekone.calculate()}!');
+}
---
+++ test/nti_weekone_test.dart
@@ -0,0 +1,8 @@
+import 'package:nti_weekone/nti_weekone.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
